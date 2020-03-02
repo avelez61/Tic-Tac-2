c8a71d2b667c32e236a1fdb24b53abdbeab0838f
avelez61 <velezaj13@gmail.com>
2020-03-01 23:39:45 -0500

    Added Making a Move and Drawing grid.rb includes the make_move function which sets the player x bitboard if the user clicks on an empty square grid.rb includes a draw function which displays the grid and any marks (only xs right now) Program shows the mouse when hovering over the window main.rb includes the button_up callback which will hold the functionality for when the game gets updated

--- grid.rb
+++ grid.rb
@@ -1,7 +1,35 @@
 class Grid
     def initialize
-        @turn = COSNT_X
+        @turn = CONST_X
         @boards = {X: 0b0_00000000, O: 0b0_00000000}
+    end
+
+    def make_move(mx, my)
+        grid_square = (my.to_i / CELL_SIZE) * 3 + (mx.to_i / CELL_SIZE)
+        digit = 2 ** grid_square
+
+        if @boards[CONST_X] & digit != digit and @boards[CONST_O] & digit != digit
+            @boards[@turn] += digit
+            return true
+        else
+            return false
+        end
+    end
+
+    def draw
+        GRID_IMAGE.draw(0, 0, 0)
+
+        for i in (0..9)
+            digit = 2 ** i
+            x_pos = i % 3 * CELL_SIZE
+            y_pos = i / 3 * CELL_SIZE
+
+            if @boards[CONST_X] & digit == digit 
+                X_IMAGE.draw(x_pos, y_pos, 0)
+            elsif @boards[CONST_O] & digit == digit
+                O_IMAGE.draw(x_pos, y_pos, 0)
+            end
+        end        
     end
 end
 
--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative "grid"
 
 # Window Constants
 CELL_SIZE = 64
-WINDOW_WIDTH = WINDOW_HEIGHT = CELL_SIZE * 3 
+WINDOW_WIDTH = WINDOW_HEIGHT = CELL_SIZE * 3
 
 # Game Constants
 GAME_OVER_STATES = [
@@ -11,7 +11,7 @@ GAME_OVER_STATES = [
     0b0_0100_1001, 0b0_1001_0010, 0b1_0010_0100, # Vertical Wins
     0b1_0001_0001, 0b0_0101_0100, 0b1_1111_1111  # Diagonal Wins + Draw State
 ]
-COSNT_X = :X
+CONST_X = :X
 CONST_O = :O
 
 # Graphics Constants
@@ -23,15 +23,25 @@ class Window < Gosu::Window
     def initialize
         super(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.caption = "Tic-Tac-Toe"
+
         @game_over = false
+        @grid = Grid.new
     end
 
-    def update
+    def needs_cursor?
+        true
+    end
 
+    def button_up(id)
+        if id == Gosu::MS_LEFT and not @game_over
+            @grid.make_move(self.mouse_x, self.mouse_y)
+        else
+            super
+        end
     end
 
     def draw
-
+        @grid.draw
     end
 end
 
