2f71592b9e30fefd2daccd91ee6223243d1decfd
avelez61 <velezaj13@gmail.com>
2020-03-01 21:44:32 -0500

    Added grid.rb grid.rb holds the grid object to hold bitboard information on the game as well as provide functionality and drawing for the board Game constants added to main.rb Name of png files got changed

---
+++ grid.rb
@@ -0,0 +1,7 @@
+class Grid
+    def initialize
+        @turn = COSNT_X
+        @boards = {X: 0b0_00000000, O: 0b0_00000000}
+    end
+end
+
--- main.rb
+++ main.rb
@@ -1,12 +1,29 @@
 require "gosu"
+require_relative "grid"
 
+# Window Constants
 CELL_SIZE = 64
 WINDOW_WIDTH = WINDOW_HEIGHT = CELL_SIZE * 3 
+
+# Game Constants
+GAME_OVER_STATES = [
+    0b0_0000_0111, 0b0_0011_1000, 0b1_1100_0000, # Horizontal Wins
+    0b0_0100_1001, 0b0_1001_0010, 0b1_0010_0100, # Vertical Wins
+    0b1_0001_0001, 0b0_0101_0100, 0b1_1111_1111  # Diagonal Wins + Draw State
+]
+COSNT_X = :X
+CONST_O = :O
+
+# Graphics Constants
+GRID_IMAGE = Gosu::Image.new("grid.png")
+X_IMAGE = Gosu::Image.new("x.png")
+O_IMAGE = Gosu::Image.new("o.png")
 
 class Window < Gosu::Window
     def initialize
         super(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.caption = "Tic-Tac-Toe"
+        @game_over = false
     end
 
     def update
@@ -14,7 +31,7 @@ class Window < Gosu::Window
     end
 
     def draw
-        
+
     end
 end
 
