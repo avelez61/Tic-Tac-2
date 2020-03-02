181566c3cc212d9a8fb91d044a137fa520baff75
avelez61 <velezaj13@gmail.com>
2020-03-01 21:21:21 -0500

    Window is Drawn

--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
 require "gosu"
 
+CELL_SIZE = 64
+WINDOW_WIDTH = WINDOW_HEIGHT = CELL_SIZE * 3 
+
 class Window < Gosu::Window
     def initialize
-
+        super(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.caption = "Tic-Tac-Toe"
     end
 
     def update
@@ -10,6 +14,8 @@ class Window < Gosu::Window
     end
 
     def draw
-
+        
     end
 end
+
+Window.new.show
